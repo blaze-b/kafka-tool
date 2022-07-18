5d2c701572e4198f36bde3641779ab97d14ddc9c
Brian Blaze <55305942+brianblaze14@users.noreply.github.com>
2022-07-18 11:32:32 +0530

    changes done in the kafdrop part for the kafka reuse tool

--- kafka_starter.bat
+++ kafka_starter.bat
@@ -1,6 +1,8 @@
 REM AUTHOR brianblaze14
 @echo off
-set KF_ROOT_PATH=D:\Applns\kafka_2.12-2.3.0
+set KF_JAVA_HOME=D:\Applns\openlogic-openjdk-11.0.8+10-windows-x64\openlogic-openjdk-11.0.8+10-win-x64\bin
+set KFDROP_PATH=./kafdrop
+set KF_ROOT_PATH=./kafka_2.12-3.2.0
 echo %KF_ROOT_PATH%
 echo "************Starting Zookeeper***********************"
 start /d  "%KF_ROOT_PATH%" bin\windows\zookeeper-server-start.bat config/zookeeper.properties
@@ -8,26 +10,24 @@ echo "************Zookeeper is up***********************"
 echo "************Starting Kafka***********************"
 pause
 start /d "%KF_ROOT_PATH%" bin\windows\kafka-server-start.bat config/server.properties
+echo "************Kafka is up***********************"
 echo "************Create topic and press any key to continue***********************"
 set /p topic=Enter Topic name: 
 echo %topic%
 pause
 echo "************Created topic***********************"
 start /d "%KF_ROOT_PATH%" bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic %topic%
-echo "Created topic Successfully"
+echo "Topic created successfully"
 pause
 echo "************Opening Producer Console***********************"
 start /d "%KF_ROOT_PATH%" bin\windows\kafka-console-producer.bat --broker-list localhost:9092 --topic %topic%
-echo "Successfully opened Producer Console"
+echo "Successfully opened kafka producer console"
 pause
-echo "************Opening Consumer Bat***********************"
+echo "************Opening Consumer Console***********************"
 start /d "%KF_ROOT_PATH%" bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic %topic% --from-beginning
-echo "Successfully opened Consumer Console"
+echo "Successfully opened kafka consumer console"
 pause
 echo "************Enabling Kafdrop Viewer***********************"
-set KF_JAVA_HOME=%KF_ROOT_PATH%\jdk-11.0.5_windows-x64_bin\bin
-set KFDROP_PATH=%KF_ROOT_PATH%\kafdrop
-echo %KFDROP_PATH%
-echo %KF_JAVA_HOME%
-start /d "%KFDROP_PATH%" %KF_JAVA_HOME%\java -jar kafdrop-3.13.0.jar
+start /d "%KFDROP_PATH%" %KF_JAVA_HOME%\java -jar kafdrop-3.30.0.jar --server.port=9001 --management.server.port=9001 --kafka.brokerConnect=localhost:9092
+echo "************Kafdrop Viewer (http://localhost:9001)***********************"
 pause
